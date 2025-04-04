14611a6872ca924dd5c313a4b52cc0f85e10aa06
aaliyahEvanie <bernice.botes42@gmail.com>
2025-04-04 19:38:43 +0200

    basic background

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { ConfigProvider } from 'antd'
+import { theme } from './theme/themeConfig.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ConfigProvider theme={theme}>
+      <App />
+    </ConfigProvider>
   </StrictMode>,
 )
---
+++ src/theme/themeConfig.ts
@@ -0,0 +1,16 @@
+import type { ThemeConfig } from 'antd/es/config-provider/context'
+
+export const theme: ThemeConfig = {
+    token: {
+        borderRadius: 8, 
+        customGreen: '#7FC249',
+        customYellow: '#CFE155',
+        customBlue: '#93D6CF',
+        customDarkGreen:'#004100',
+
+
+    }, 
+    components: {
+
+    }
+}
